de4e9474aef624b378286f51ae2adf4ffbab4134
pari Vaish <pariv7006@gmail.com>
2026-09-06 00:24:53 +0530

    Complete Day:5 frontend setup

---
+++ client/src/utils/constants.js
@@ -0,0 +1,30 @@
+export const TECH_STACKS = [
+  "JavaScript",
+  "React",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+  "Java",
+  "Python",
+  "C++",
+  "SQL",
+];
+
+export const JOB_ROLES = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Full Stack Developer",
+  "Software Developer",
+  "Java Developer",
+  "React Developer",
+  "Data Analyst",
+  "Software Engineer",
+];
+
+export const EXPERIENCE_LEVELS = [
+  "Fresher",
+  "0-1 Years",
+  "1-3 Years",
+  "3-5 Years",
+  "5+ Years",
+];
